0c82bc81b4bf4b574b4a241ac4ee09cf804cc3a5
s207017 <64325566+s207017@users.noreply.github.com>
2022-09-26 23:26:13 +0800

    monthlymood

--- src/server/functions/routes/firebase-auth.js
+++ src/server/functions/routes/firebase-auth.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin')
 
 async function checkauthenticated(req, res, next){
     let token = req.cookies['auth_token']
-    console.log(token)
+    //console.log(token)
     admin.auth().verifyIdToken(token).then((decodedtoken) => {
         const uid = decodedtoken.uid
         req.body.uid = uid
--- src/server/functions/routes/monthlymood.js
+++ src/server/functions/routes/monthlymood.js
@@ -5,47 +5,50 @@ const moment = require('moment')
 async function monthlymood(req, res){
     const uid = req.body.uid
     const currentDate = moment().format("DD-MM-YYYY").split("-")
-    const next = moment().add(1, 'months').format("DD-MM-YYYY")
+    const next = moment().add(1, 'month').format("DD-MM-YYYY").split("-")
     const nextMonthYear = next[2]
     const nextMonth = next[1]
-    const currentDay = currentDate[0]
     const currentMonth = currentDate[1]
     const currentYear = currentDate[2]
 
-    try {
-        var snapshot = await firestore
-        .collection('users')
-        .doc(userID)
-        .collection('journal')
-        .where(
-            'timestamp',
-            ">=",
-            new Date(`${currentYear}-${currentMonth}-01`)
-        )
-        .where(
-            'timestamp',
-            "<",
-            new Date(`${nextMonthYear}-${nextMonth}-01`)
-        )
-        .select('sentiment')
-        .get()
 
-        if (!snapshot.exists){
-            return res.status(400).json({
-                success: false,
-                error: 'journals not found in that month'
+    const query = firestore.collection(`users/${uid}/journal`)
+                    .where('fields.timestamp',
+                            '>=',
+                            new Date(`${currentYear}-${currentMonth}-01`))
+                    .where('fields.timestamp',
+                            '<',
+                            new Date(`${nextMonthYear}-${nextMonth}`))
+    await query.onSnapshot(querysnapshot => {
+        var moods = []
+        querysnapshot.forEach((doc) => {
+            console.log(doc.data())
+            moods.push({
+                timestamp: doc.data().fields.timestamp,
+                sentiment: doc.data().fields.sentiment
             })
-        }
-        const moods = snapshot.data()
-        return res.status(400).json({
+        })
+        res.status(200).json({
             moods
         })
-    } catch(err){
+    }, err => {
         res.status(400).json({
             success: false,
-            error: err
+            error: err.message
         })
-    } 
+    })
+
+        // if (!snapshot.exists){
+        //     return res.status(400).json({
+        //         success: false,
+        //         error: 'journals not found in that month'
+        //     })
+        // }
+        // const moods = snapshot.data()
+        // return res.status(400).json({
+        //     moods
+        // })
+    
 
 
 }
