cca297c1cf92eaaf71d8c72cd2e3650037470618
s207017 <64325566+s207017@users.noreply.github.com>
2022-10-06 22:12:10 +0800

    justsentiment

--- src/server/functions/routes/firebase-auth.js
+++ src/server/functions/routes/firebase-auth.js
@@ -2,11 +2,13 @@ const {getAuth} = require('firebase-admin/auth');
 const admin = require('firebase-admin')
 
 async function checkauthenticated(req, res, next){
+    console.log('withinauth!')
     let token = req.cookies['auth_token']
     //console.log(token)
     admin.auth().verifyIdToken(token).then((decodedtoken) => {
         const uid = decodedtoken.uid
         req.body.uid = uid
+        console.log(req.body.uid)
         next()
     }).catch((error) => {
         return res.status(401).json({
