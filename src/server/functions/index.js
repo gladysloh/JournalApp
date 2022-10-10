44f318e6f6721b275e0a11fac96eb307608a7256
s207017 <64325566+s207017@users.noreply.github.com>
2022-10-10 20:03:58 +0800

    d

--- src/server/functions/index.js
+++ src/server/functions/index.js
@@ -1,48 +1,48 @@
 const functions = require("firebase-functions");
-const express = require('express')
+const express = require("express");
 const session = require('express-session')
-const admin = require('firebase-admin')
-const validateEmailAndPassword = require('./routes/validate')
+const admin = require('firebase-admin');
+const validateEmailAndPassword = require('./routes/validate');
 const firebaseConfig = { apiKey: "AIzaSyB5-qTKTZqSYmhnTvGo7xRp-tPgS8bqQ0s",
                             authDomain: "onceaday-48fb7.firebaseapp.com",
                             projectId: "onceaday-48fb7",
                             storageBucket: "onceaday-48fb7.appspot.com",
                             messagingSenderId: "1079672258338",
                             appId: "1:1079672258338:web:0659c5e7f2974ed80a2f7b"
-}
-const { initializeApp } = require('firebase/app')
-const serviceAccount = require('../serviceAccountKey.json')
-const cors = require('cors')
+};
+const { initializeApp } = require('firebase/app');
+const serviceAccount = require('../serviceAccountKey.json');
+const cors = require('cors');
 const morgan = require('morgan');
-const cookieparser = require('cookie-parser')
-const bodyparser = require('body-parser')
+const cookieparser = require('cookie-parser');
+const bodyparser = require('body-parser');
 
-const { Firestore } = require('@google-cloud/firestore')
-const { FirestoreStore } = require('@google-cloud/connect-firestore')
+const { Firestore } = require('@google-cloud/firestore');
+const { FirestoreStore } = require('@google-cloud/connect-firestore');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: "onceaday-48fb7.appspot.com"
-})
-initializeApp(firebaseConfig)
+});
+initializeApp(firebaseConfig);
 
-const register = require('./routes/register')
-const login = require('./routes/login')
-const firebaseAuth = require('./routes/firebase-auth')
-const getUser = require('./routes/get-user')
+const register = require('./routes/register');
+const login = require('./routes/login');
+const firebaseAuth = require('./routes/firebase-auth');
+const getUser = require('./routes/get-user');
 const googlesignin = require('./routes/googlesignup');
 const createjournal = require("./routes/createjournal");
 const getalljournal = require('./routes/getalljournal');
 const getRandomQuestion = require("./routes/getrandomquestion");
-const editjournal = require('./routes/editjournal')
-const logout = require('./routes/logout')
+const editjournal = require('./routes/editjournal');
+const logout = require('./routes/logout');
 const sentimentAnalyzer = require('./routes/sentimentAnalyzer');
 const googlelogin = require("./routes/googlesignup");
 const monthlymood = require('./routes/monthlymood');
 const removejournal = require("./routes/removejournal");
 const justsentiment = require("./routes/justsentiment");
 const getonejournal = require("./routes/getonejournal");
-const app = express()
+const app = express();
 app.use(session({
     store: new FirestoreStore({
         dataset: new Firestore(),
@@ -56,7 +56,7 @@ app.use(session({
         maxAge: 3600000,
         expires: new Date(Date.now() + 3600000) 
     }
-}))//what the fuck
+}));//what the fuck;
 app.use(cors({
     // origin: true,
     credentials: true,
@@ -72,19 +72,19 @@ app.use((req, res, next) => {
     );
     next();
   });
-app.use(cookieparser())
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended: true}))
+app.use(cookieparser());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
 
-app.post('/login', validateEmailAndPassword, login)
-app.post('/register', validateEmailAndPassword, register)
-app.get('/getuser', firebaseAuth, getUser)
-app.post('/createjournal', firebaseAuth, createjournal)
-app.get('/getrandomquestion', getRandomQuestion)
-app.post('/editjournal', firebaseAuth, editjournal)
-app.post('/logout', logout)
-app.get('/randomq', getRandomQuestion)
+app.post('/login', validateEmailAndPassword, login);
+app.post('/register', validateEmailAndPassword, register);
+app.get('/getuser', firebaseAuth, getUser);
+app.post('/createjournal', firebaseAuth, createjournal);
+app.get('/getrandomquestion', getRandomQuestion);
+app.post('/editjournal', firebaseAuth, editjournal);
+app.post('/logout', logout);
+app.get('/randomq', getRandomQuestion);
 app.post('/sentimentanalyzer', sentimentAnalyzer)
 app.post('/googlelogin', googlelogin)
 app.post('/monthlymood', firebaseAuth, monthlymood)
